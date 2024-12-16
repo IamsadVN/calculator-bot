0f80f361b6e6c4370e687c70f17a26bd012bb967
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2024-12-16 20:53:19 +0700

    mysql migrate: update debug log (again :droplet:)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import { config } from "dotenv";
 import { Client, GatewayIntentBits, Collection } from "discord.js";
 import { loadEvents, loadCommands } from "./utils/loader.js";
-import { errorLog, infoLog } from "./utils/log.js"
+import { errorLog, infoLog, warnLog } from "./utils/log.js"
 import connection from "../database/schema.js";
 import i18next from "i18next";
 import Backend from "i18next-fs-backend";
 
 //env
 config();
+
+//Debug log warning
+if (process.env.DEBUG_LOG === "true") 
+    warnLog("Debug log is on, turn off debug log if you forgot config the .env file");
 
 //i18next 
 i18next.use(Backend).init({
