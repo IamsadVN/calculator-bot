c4feaf3ee28ce9eadd6a6667cb0492b1af214ed5
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2024-12-19 22:24:47 +0700

    mysql migrate: update lang command

--- src/commands/utils/lang.js
+++ src/commands/utils/lang.js
@@ -1,54 +1,77 @@
 import { SlashCommandBuilder } from "discord.js";
-import langOfEachGuild from "../../../schemas/LangOfGuild.js";
 import { getLang } from "../../utils/getLang.js";
+import { ServerSetting, UserSetting } from "../../../database/schema.js";
 
 export default {
     name: 'language',
     description: 'Chỉnh ngôn ngữ của máy tính - Change the language (Only using ISO-639)',
     aliases: ["lang"],
 
+    /**
+     * 
+     * @param {import("discord.js").Message<true>} message 
+     * @param {string[] | undefined} args 
+     * @param {import("i18next").i18n} i18next 
+     * @returns 
+     */
     async executeMessage(message, args, i18next) {
         const langCode = args[0];
-        const language = await getLang(message)
+        const language = await getLang(message);
 
-        let guildToChangeLang = await langOfEachGuild.findOne({
-            guildID: message.guild.id
-        });
+        if (langCode === undefined) {
+            const serverLang = await new ServerSetting().getServerLang(message.guildId);
 
-        if (!guildToChangeLang) {
-            guildToChangeLang = new langOfEachGuild({
-                guildID: message.guild.id
+            await message.channel.send({
+                content: i18next.t("lang.currentLang",{lng: language, currentLang: serverLang})
             });
         }
+        else if (langCode.length > 2) {
+            await message.channel.send({
+                content: i18next.t("lang.error.isNotLangCode",{lng:language})
+            });
+            return;
+        }
 
-        guildToChangeLang.lang = langCode;
-        await guildToChangeLang.save();
+        await new ServerSetting().writeServerLang({
+            servername: message.guild.name,
+            serverid: message.guildId,
+            serverlang: langCode,
+            servermembers: message.guild.memberCount,
+            serverowner: (await message.guild.fetchOwner(message.guild.ownerId)).user.username,
+            serverownerid: message.guild.ownerId
+        });
 
-        message.channel.send(i18next.t("lang.changedLang", { language: langCode, lng: language }))
+        message.channel.send(i18next.t("lang.changedLang", { language: langCode, lng: language }));
     },
 
+    /**
+     * 
+     * @param {import("discord.js").ChatInputCommandInteraction} interaction 
+     * @param {import("i18next").i18n} i18next 
+     */
     async executeChatInput(interaction, i18next) {
         const langCode = interaction.options.getString("lang");
-
-        const language = await getLang(interaction)
+        const language = await getLang(interaction);
 
         await interaction.deferReply();
 
-        let guildToChangeLang = await langOfEachGuild.findOne({
-            guildID: interaction.guild.id,
-        });
-        //Check xem guildID đã có hay chưa, nếu chưa
-        //Thì tạo 
-        if (!guildToChangeLang) {
-            guildToChangeLang = new langOfEachGuild({
-                guildID: interaction.guild.id
-            })
+        if (interaction.inCachedGuild()) {
+            await new ServerSetting().writeServerLang({
+                serverid: interaction.guildId,
+                serverlang: langCode,
+                servermembers: interaction.guild.memberCount,
+                servername: interaction.guild.name,
+                serverowner: (await interaction.guild.fetchOwner(interaction.guild.ownerId)).user.username,
+                serverownerid: interaction.guild.ownerId
+            });
         }
-        //Có nghĩa rằng sau khi check xem đã có hay chưa, giờ
-        //Sẽ add thêm lang vào
-        guildToChangeLang.lang = langCode;
-
-        await guildToChangeLang.save();
+        else {
+            await new UserSetting().writeUserLang({
+               userId: interaction.user.id,
+               userlang: langCode,
+               username: interaction.user.username
+            });
+        }
 
         interaction.editReply(i18next.t("lang.changedLang", {language: langCode, lng: language }));
     },
@@ -57,6 +80,8 @@ export default {
         commands.push(new SlashCommandBuilder()
             .setName(this.name)
             .setDescription(this.description)
+            .setContexts([0,2])
+            .setIntegrationTypes([0,1])
             .addStringOption(option =>
                 option.setName("lang")
                     .setDescription("Ngôn ngữ mà bạn muốn thay đổi")
